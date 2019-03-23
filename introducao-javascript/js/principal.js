a2862e68dfac4d3440042df306daf5a800907624
Vinícius Melo <viniciusdesk@icloud.com>
2019-03-23 12:19:04 -0300

    Addition of calculations and styles in js.

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,3 +1,38 @@
 var titulo = document.querySelector(".titulo");
 
 titulo.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll('.paciente');
+
+for (var i = 0; i < pacientes.length; i++) {
+
+    var paciente = pacientes[i]
+    var tdPeso = paciente.querySelector(".info-peso");
+    var tdAltura = paciente.querySelector(".info-altura");
+    
+    var peso = tdPeso.textContent;
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
+    var alturaEhValida = true;
+    var pesoEhValido = true;
+
+    if (peso <= 0 || peso >= 1000) {
+        tdPeso.textContent = "Peso inválido!";
+        pesoEhValido = false;
+        paciente.classList.add('paciente-invalido');
+    }
+
+    if (altura <= 0 || altura >= 3.0) {
+        tdAltura.textContent = "Altura inválida!";
+        alturaEhValida = false;
+        paciente.classList.add('paciente-invalido');
+    }
+
+    if (alturaEhValida && pesoEhValido) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    } else {
+        tdImc.textContent = "Altura ou peso inválidos!";
+    }
+}
