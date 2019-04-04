e8e9b3a46bd9bea7efa1d3cc696ef0f6db7b592b
Vinícius Melo <viniciusdesk@icloud.com>
2019-04-03 21:12:27 -0300

    Improved imc calculation and form event driven.

--- introducao-javascript/js/calcula-imc.js
+++ introducao-javascript/js/calcula-imc.js
@@ -30,43 +30,14 @@ for (var i = 0; i < pacientes.length; i++) {
   }
 
   if (alturaEhValida && pesoEhValido) {
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc.toFixed(2);
+    var imc = calculaImc(peso, altura);
+    tdImc.textContent = imc;
   } else {
     tdImc.textContent = "Altura ou peso inválidos!";
   }
 }
 
-var botaoAdicionar = document.querySelector("#adicionar-paciente");
-
-botaoAdicionar.addEventListener("click", function(event) {
-  event.preventDefault();
-  var form = document.querySelector("#form-adiciona");
-
-  var nome = form.nome.value;
-  var peso = form.peso.value;
-  var altura = form.altura.value;
-  var gordura = form.gordura.value;
-
-  var pacienteTr = document.createElement("tr");
-
-  var nomeTd = document.createElement("td");
-  var pesoTd = document.createElement("td");
-  var alturaTd = document.createElement("td");
-  var gorduraTd = document.createElement("td");
-  var imcTd = document.createElement("td");
-
-  nomeTd.textContent = nome;
-  pesoTd.textContent = peso;
-  alturaTd.textContent = altura;
-  gorduraTd.textContent = gordura;
-
-  pacienteTr.appendChild(nomeTd);
-  pacienteTr.appendChild(pesoTd);
-  pacienteTr.appendChild(alturaTd);
-  pacienteTr.appendChild(gorduraTd);
-
-  var tabela = document.querySelector("#tabela-pacientes");
-
-  tabela.appendChild(pacienteTr);
-});
+function calculaImc(peso, altura) {
+  var imc = peso / (altura * altura);
+  return imc.toFixed(2);
+}
