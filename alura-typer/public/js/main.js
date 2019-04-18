2b0b820927decd2f2ff4eb8b5f5d3df195f72c23
Vinícius Melo <viniciusdesk@icloud.com>
2019-04-17 23:20:14 -0300

    Now counting words and letters of input.

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -3,3 +3,14 @@ var numPalavras = frase.split(" ").length;
 
 var tamanhoFrase = $("#tamanho-frase");
 tamanhoFrase.text(numPalavras);
+
+var campo = $(".campo-digitacao");
+campo.on("input", function() {
+    var conteudo = campo.val();
+    var qtdPalavras = conteudo.split(/\S+/).length - 1;
+    $('#contador-palavras').text(qtdPalavras);
+
+    var conteudoSemEspaco = conteudo.replace(/\S+/, '');
+    var qtdCaracteres = conteudoSemEspaco.length;
+    $('#contador-caracteres').text(qtdCaracteres);
+});
