42ab17ab3ddaf44da5a5480ff4246590029012bd
Vinícius Melo <viniciusdesk@icloud.com>
2019-04-19 11:56:04 -0300

    Code refactored and addition of more control over the input.

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -1,16 +1,56 @@
-var frase = $(".frase").text();
-var numPalavras = frase.split(" ").length;
-
-var tamanhoFrase = $("#tamanho-frase");
-tamanhoFrase.text(numPalavras);
-
+var tempoInicial = $('#tempo-digitacao').text();
 var campo = $(".campo-digitacao");
-campo.on("input", function() {
-    var conteudo = campo.val();
-    var qtdPalavras = conteudo.split(/\S+/).length - 1;
-    $('#contador-palavras').text(qtdPalavras);
 
-    var conteudoSemEspaco = conteudo.replace(/\S+/, '');
-    var qtdCaracteres = conteudoSemEspaco.length;
-    $('#contador-caracteres').text(qtdCaracteres);
+$(function() {
+    atualizaTamanhoFrase();
+    inicializaContadores();
+    inicializaCronometro();
+    $('#botao-reiniciar').click(reiniciaJogo);
 });
+
+function atualizaTamanhoFrase() {
+    var frase = $(".frase").text();
+    var numPalavras = frase.split(" ").length;
+    
+    var tamanhoFrase = $("#tamanho-frase");
+    tamanhoFrase.text(numPalavras);
+}
+
+function inicializaContadores() {
+    campo.on("input", function() {
+        var conteudo = campo.val();
+        var qtdPalavras = conteudo.split(/\S+/).length - 1;
+        $('#contador-palavras').text(qtdPalavras);
+    
+        var conteudoSemEspaco = conteudo.replace(/\S+/, '');
+        var qtdCaracteres = conteudoSemEspaco.length;
+        $('#contador-caracteres').text(qtdCaracteres);
+    });
+}
+
+function inicializaCronometro() {
+    var tempoRestante = $('#tempo-digitacao').text();
+    campo.one("focus", function() {
+        campo.val("");
+        $('#botao-reiniciar').attr("disabled", true);
+        var cronometroID = setInterval(function() {
+            tempoRestante--;
+            $('#tempo-digitacao').text(tempoRestante);
+            if (tempoRestante < 1) {
+                campo.attr("disabled", true);
+                clearInterval(cronometroID);
+                $('#botao-reiniciar').attr("disabled", false);
+            }
+        }, 1000);
+    });
+}
+
+function reiniciaJogo() {
+    campo.attr('disabled', false);
+    campo.val('');
+    $('#contador-palavras').text("0");
+    $('#contador-caracteres').text("0");
+    $("#tempo-digitacao").text(tempoInicial);
+    
+    inicializaCronometro();
+}
