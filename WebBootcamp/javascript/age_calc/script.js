9bd664042799f62eef061ae665794882295b2d58
Vinicius Melo <vinimmelo.dev@gmail.com>
2020-02-21 10:57:45 -0300

    Addition of javascript challenges

---
+++ WebBootcamp/javascript/age_calc/script.js
@@ -0,0 +1,7 @@
+console.log("Conectado");
+
+var age = prompt('How old are you?');
+
+var days = age * 365;
+
+alert(`${age} years is roughly ${days} days`);
---
+++ WebBootcamp/javascript/js_stalker/stalker.js
@@ -0,0 +1,6 @@
+fName = prompt('What is your first name?');
+lName = prompt('What is your last name?');
+age = prompt('What is your age?');
+
+console.log(`Your full name is ${fName} ${lName}`);
+console.log(`You are ${age} years old`);
